47a6fc4d238ae708d76dd1e5214e529857dd7155
chirag1126 <chiragbsanil07@gmail.com>
2022-09-25 23:09:46 +0530

    My Project

---
+++ users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_view
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('ping/', views.ping, name='ping'),
+    path('aboutus/', views.aboutus, name='aboutus'),
+    path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+]
